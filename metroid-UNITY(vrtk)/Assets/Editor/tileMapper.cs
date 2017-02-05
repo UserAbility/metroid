88e3b31d5893c47bf981bc6e4111c53eae844061
Rob Mocca <@gmail.com>
2017-02-04 18:08:19 -0600

    Attempt to branch correctly

---
+++ metroid-UNITY(vrtk)/Assets/Editor/tileMapper.cs
@@ -0,0 +1,15 @@
+﻿
+public class tileMapperList
+{
+    public string tileByte { get; set; }
+    public string preFabName { get; set; }
+}
+
+
+public class structList
+{
+    public string structureName { get; set; }
+    public string tileByte { get; set; }
+ 
+
+}
